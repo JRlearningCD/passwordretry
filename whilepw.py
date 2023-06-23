35cff75f7de2ace9d301fe6ec05d94b33bd02965
JRlearningCD <dennis101168@gmail.com>
2023-06-23 12:00:01 +0800

    first commit

---
+++ whilepw.py
@@ -0,0 +1,20 @@
+password = "a123456"
+passwordin = input("請輸入密碼，可嘗試次數3次")
+X = 3
+X = int(X)
+if passwordin == password:
+	print("歡迎回來")
+else:
+	X=X-1
+	while 0 < X < 3:
+		print("密碼錯誤，請重新輸入，可輸入次數為：")
+		print(X)
+		passwordin1 = input()
+		if passwordin1 == password:
+			print("歡迎回來")
+			break
+		else: 
+			X=X-1	
+	while X == 0:
+		print(".....限制進入.....")
+		break
